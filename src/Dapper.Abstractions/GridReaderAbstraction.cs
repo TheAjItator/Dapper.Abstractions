7d69b0b16d0c8c00e1e29b77869443e1b2d88d0b
Mark Johnson <mark@twistersdomain.co.uk>
2018-04-29 17:33:05 +0100

    Added GridReader Async methods

--- src/Dapper.Abstractions/GridReaderAbstraction.cs
+++ src/Dapper.Abstractions/GridReaderAbstraction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Dapper.Abstractions
 {
@@ -12,9 +13,31 @@ namespace Dapper.Abstractions
             _gridReader = gridReader;
         }
 
+        #region Sync Methods
+
         public IEnumerable<dynamic> Read(bool buffered = true)
         {
             return _gridReader.Read(buffered);
+        }
+
+        public dynamic ReadFirst()
+        {
+            return _gridReader.ReadFirst();
+        }
+
+        public dynamic ReadFirstOrDefault()
+        {
+            return _gridReader.ReadFirstOrDefault();
+        }
+
+        public dynamic ReadSingle()
+        {
+            return _gridReader.ReadSingle();
+        }
+
+        public dynamic ReadSingleOrDefault()
+        {
+            return _gridReader.ReadSingleOrDefault();
         }
 
         public IEnumerable<T> Read<T>(bool buffered = true)
@@ -22,25 +45,166 @@ namespace Dapper.Abstractions
             return _gridReader.Read<T>(buffered);
         }
 
-        public IEnumerable<TReturn> Read<TFirst, TSecond, TReturn>(Func<TFirst, TSecond, TReturn> func, string splitOn = "id")
+        public T ReadFirst<T>()
         {
-            return _gridReader.Read(func, splitOn);
+            return _gridReader.ReadFirst<T>();
         }
 
-        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TReturn>(Func<TFirst, TSecond, TThird, TReturn> func, string splitOn = "id")
+        public T ReadFirstOrDefault<T>()
         {
-            return _gridReader.Read(func, splitOn);
+            return _gridReader.ReadFirstOrDefault<T>();
         }
 
-        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TReturn> func, string splitOn = "id")
+        public T ReadSingle<T>()
         {
-            return _gridReader.Read(func, splitOn);
+            return _gridReader.ReadSingle<T>();
         }
 
-        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> func, string splitOn = "id")
+        public T ReadSingleOrDefault<T>()
         {
-            return _gridReader.Read(func, splitOn);
+            return _gridReader.ReadSingleOrDefault<T>();
         }
+
+        public IEnumerable<object> Read(Type type, bool buffered = true)
+        {
+            return _gridReader.Read(type, buffered);
+        }
+
+        public object ReadFirst(Type type)
+        {
+            return _gridReader.ReadFirst(type);
+        }
+
+        public object ReadFirstOrDefault(Type type)
+        {
+            return _gridReader.ReadFirstOrDefault(type);
+        }
+
+        public object ReadSingle(Type type)
+        {
+            return _gridReader.ReadSingle(type);
+        }
+
+        public object ReadSingleOrDefault(Type type)
+        {
+            return _gridReader.ReadSingleOrDefault(type);
+        }
+
+        public IEnumerable<TReturn> Read<TFirst, TSecond, TReturn>(Func<TFirst, TSecond, TReturn> func, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(func, splitOn, buffered);
+        }
+
+        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TReturn>(Func<TFirst, TSecond, TThird, TReturn> func, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(func, splitOn, buffered);
+        }
+
+        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TReturn> func, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(func, splitOn, buffered);
+        }
+
+        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> func, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(func, splitOn, buffered);
+        }
+
+        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn> func, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(func, splitOn, buffered);
+        }
+
+        public IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn> func, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(func, splitOn, buffered);
+        }
+
+        public IEnumerable<TReturn> Read<TReturn>(Type[] types, Func<object[], TReturn> map, string splitOn = "id", bool buffered = true)
+        {
+            return _gridReader.Read(types, map, splitOn, buffered);
+        }
+
+        #endregion Sync Methods
+
+        #region Async Methods
+
+        public Task<IEnumerable<dynamic>> ReadAsync(bool buffered = true)
+        {
+            return _gridReader.ReadAsync(buffered);
+        }
+
+        public Task<dynamic> ReadFirstAsync()
+        {
+            return _gridReader.ReadFirstAsync();
+        }
+
+        public Task<dynamic> ReadFirstOrDefaultAsync()
+        {
+            return _gridReader.ReadFirstOrDefaultAsync();
+        }
+
+        public Task<dynamic> ReadSingleAsync()
+        {
+            return _gridReader.ReadSingleAsync();
+        }
+
+        public Task<dynamic> ReadSingleOrDefaultAsync()
+        {
+            return _gridReader.ReadSingleOrDefaultAsync();
+        }
+
+        public Task<IEnumerable<object>> ReadAsync(Type type, bool buffered = true)
+        {
+            return _gridReader.ReadAsync(type, buffered);
+        }
+
+        public Task<object> ReadFirstAsync(Type type)
+        {
+            return _gridReader.ReadFirstAsync(type);
+        }
+
+        public Task<object> ReadFirstOrDefaultAsync(Type type)
+        {
+            return _gridReader.ReadFirstOrDefaultAsync(type);
+        }
+
+        public Task<object> ReadSingleAsync(Type type)
+        {
+            return _gridReader.ReadSingleAsync(type);
+        }
+
+        public Task<object> ReadSingleOrDefaultAsync(Type type)
+        {
+            return _gridReader.ReadSingleOrDefaultAsync(type);
+        }
+
+        public Task<IEnumerable<T>> ReadAsync<T>(bool buffered = true)
+        {
+            return _gridReader.ReadAsync<T>(buffered);
+        }
+
+        public Task<T> ReadFirstAsync<T>()
+        {
+            return _gridReader.ReadFirstAsync<T>();
+        }
+
+        public Task<T> ReadFirstOrDefaultAsync<T>()
+        {
+            return _gridReader.ReadFirstOrDefaultAsync<T>();
+        }
+
+        public Task<T> ReadSingleAsync<T>()
+        {
+            return _gridReader.ReadSingleAsync<T>();
+        }
+
+        public Task<T> ReadSingleOrDefaultAsync<T>()
+        {
+            return _gridReader.ReadSingleOrDefaultAsync<T>();
+        }
+
+        #endregion Async Methods
 
         public void Dispose()
         {
--- src/Dapper.Abstractions/IGridReader.cs
+++ src/Dapper.Abstractions/IGridReader.cs
@@ -1,20 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Dapper.Abstractions
 {
     public interface IGridReader : IDisposable
     {
+        #region Sync Methods
+
         IEnumerable<dynamic> Read(bool buffered = true);
+
+        dynamic ReadFirst();
+
+        dynamic ReadFirstOrDefault();
+
+        dynamic ReadSingle();
+
+        dynamic ReadSingleOrDefault();
 
         IEnumerable<T> Read<T>(bool buffered = true);
 
-        IEnumerable<TReturn> Read<TFirst, TSecond, TReturn>(Func<TFirst, TSecond, TReturn> func, string splitOn = "id");
+        T ReadFirst<T>();
 
-        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TReturn>(Func<TFirst, TSecond, TThird, TReturn> func, string splitOn = "id");
+        T ReadFirstOrDefault<T>();
 
-        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TReturn> func, string splitOn = "id");
+        T ReadSingle<T>();
 
-        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> func, string splitOn = "id");
+        T ReadSingleOrDefault<T>();
+
+        IEnumerable<object> Read(Type type, bool buffered = true);
+
+        object ReadFirst(Type type);
+
+        object ReadFirstOrDefault(Type type);
+
+        object ReadSingle(Type type);
+
+        object ReadSingleOrDefault(Type type);
+
+        IEnumerable<TReturn> Read<TFirst, TSecond, TReturn>(Func<TFirst, TSecond, TReturn> func, string splitOn = "id", bool buffered = true);
+
+        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TReturn>(Func<TFirst, TSecond, TThird, TReturn> func, string splitOn = "id", bool buffered = true);
+
+        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TReturn> func, string splitOn = "id", bool buffered = true);
+
+        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> func, string splitOn = "id", bool buffered = true);
+
+        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn> func, string splitOn = "id", bool buffered = true);
+
+        IEnumerable<TReturn> Read<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn>(Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn> func, string splitOn = "id", bool buffered = true);
+
+        IEnumerable<TReturn> Read<TReturn>(Type[] types, Func<object[], TReturn> map, string splitOn = "id", bool buffered = true);
+
+        #endregion Sync Methods
+
+        #region Async Methods
+
+        Task<IEnumerable<dynamic>> ReadAsync(bool buffered = true);
+
+        Task<dynamic> ReadFirstAsync();
+
+        Task<dynamic> ReadFirstOrDefaultAsync();
+
+        Task<dynamic> ReadSingleAsync();
+
+        Task<dynamic> ReadSingleOrDefaultAsync();
+
+        Task<IEnumerable<object>> ReadAsync(Type type, bool buffered = true);
+
+        Task<object> ReadFirstAsync(Type type);
+
+        Task<object> ReadFirstOrDefaultAsync(Type type);
+
+        Task<object> ReadSingleAsync(Type type);
+
+        Task<object> ReadSingleOrDefaultAsync(Type type);
+
+        Task<IEnumerable<T>> ReadAsync<T>(bool buffered = true);
+
+        Task<T> ReadFirstAsync<T>();
+
+        Task<T> ReadFirstOrDefaultAsync<T>();
+
+        Task<T> ReadSingleAsync<T>();
+
+        Task<T> ReadSingleOrDefaultAsync<T>();
+
+        #endregion Async Methods
     }
 }
