0225acb089dcfb97e7e239f890d31de3621b97a6
TheAjItator <1778362+TheAjItator@users.noreply.github.com>
2023-10-26 17:14:56 -0500

    Create SqlExecutorFactory.cs

---
+++ src/Dapper.Abstractions.Sql/SqlExecutorFactory.cs
@@ -0,0 +1,36 @@
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Dapper.Abstractions.Sql {
+    public class SqlExecutorFactory : IDbExecutorFactory {
+        #region Members
+
+        private readonly string _connectionString;
+
+        #endregion Members
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="connectionString">The connection for the required database</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public SqlExecutorFactory(string connectionString) {
+            if (string.IsNullOrWhiteSpace(connectionString)) {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Create an open database connection
+        /// </summary>
+        /// <returns>The newly created database connection</returns>
+        public IDbExecutor CreateExecutor() {
+            var dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+
+            return new DbExecutor(dbConnection);
+        }
+    }
+}
